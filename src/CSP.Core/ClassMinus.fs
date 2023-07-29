a34bff1ef257ff69458209314ba7762607540577
Kuniwak <orga.chem.job@gmail.com>
2023-07-30 06:59:02 +0900

    Fix overflow

--- src/CSP.Core/ClassMinus.fs
+++ src/CSP.Core/ClassMinus.fs
@@ -17,6 +17,6 @@ let rec minus (v1: Val) (v2: Val) : Val =
     match v1, v2 with
     | VUnit, VUnit -> VUnit
     | VTuple(vL1, vR1), VTuple(vL2, vR2) -> VTuple(minus vL1 vL2, minus vR1 vR2)
-    | VNat n1, VNat n2 -> VNat(n1 - n2)
+    | VNat n1, VNat n2 -> VNat(if n1 < n2 then 0u else n1 - n2)
     | VSet n1, VSet n2 -> VSet(Set.difference n1 n2)
     | _ -> failwith $"cannot minus: %s{format v1} vs %s{format v2}"
